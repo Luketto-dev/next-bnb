891b9a4515d37082dddadb9a38b0798df41c9c8a
Luketto-dev <luca.francesconi1995@gmail.com>
2022-12-19 10:04:41 +0100

    modify schema prisma

--- prisma/migrations/20221216150345_create_new_migration/migration.sql
+++ prisma/migrations/20221216150345_create_new_migration/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
-    `password` VARCHAR(40) NOT NULL,
+    `password` VARCHAR(500) NOT NULL,
     `firstname` VARCHAR(150) NULL,
     `lastname` VARCHAR(150) NULL,
 
@@ -15,10 +15,10 @@ CREATE TABLE `Apartment` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `city` VARCHAR(191) NOT NULL,
-    `price` DECIMAL(65, 30) NOT NULL,
+    `price` DOUBLE NOT NULL,
+    `type_id` INTEGER NOT NULL,
     `user_id` INTEGER NOT NULL,
 
-    UNIQUE INDEX `Apartment_user_id_key`(`user_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -37,27 +37,14 @@ CREATE TABLE `Message` (
     `content` VARCHAR(500) NOT NULL,
     `user_id` INTEGER NOT NULL,
 
-    UNIQUE INDEX `Message_user_id_key`(`user_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- CreateTable
-CREATE TABLE `_ApartmentToType` (
-    `A` INTEGER NOT NULL,
-    `B` INTEGER NOT NULL,
-
-    UNIQUE INDEX `_ApartmentToType_AB_unique`(`A`, `B`),
-    INDEX `_ApartmentToType_B_index`(`B`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- AddForeignKey
+ALTER TABLE `Apartment` ADD CONSTRAINT `Apartment_type_id_fkey` FOREIGN KEY (`type_id`) REFERENCES `Type`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Apartment` ADD CONSTRAINT `Apartment_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Message` ADD CONSTRAINT `Message_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `_ApartmentToType` ADD CONSTRAINT `_ApartmentToType_A_fkey` FOREIGN KEY (`A`) REFERENCES `Apartment`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `_ApartmentToType` ADD CONSTRAINT `_ApartmentToType_B_fkey` FOREIGN KEY (`B`) REFERENCES `Type`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
